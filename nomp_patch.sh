5be715a740060537847e57cb2d7c65085a600ed7
jacknab <jacknabvoip@gmail.com>
2024-09-29 15:52:07 -0600

    Update nomp_patch.sh

--- nomp_patch.sh
+++ nomp_patch.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#########################################################
+# Created by jacknab for php-mpos and nomp pool...
+# This script is intended to be run on Debian 11
+# like this:
+# curl https://raw.githubusercontent.com/jacknab/scripts/main/install.sh | bash
+#
+#########################################################
 
 # File 1: /root/nomp/libs/profitSwitch.js
 # Replace line 545
@@ -12,4 +19,6 @@ sed -i '122s|.*|_this.daemon.cmd('\'\'getblocktemplate'\'', [{"capabilities": [ 
 sed -i '580s|.*|[{"capabilities": [ "coinbasetxn", "workid", "coinbase/append" ], "rules": [ "mweb", "segwit" ]}],|' /root/nomp/node_modules/stratum-pool/lib/pool.js
 
 echo "Replacements completed successfully!"
+
+bash /root/scripts/update_mpos_config.sh
 
