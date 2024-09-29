94886d133cdf1fc3b10afe0f538d1b42db5d71fe
jacknab <jacknabvoip@gmail.com>
2024-09-29 16:06:38 -0600

    Update nomp.sh

--- nomp.sh
+++ nomp.sh
@@ -9,11 +9,11 @@
 
 # Install NOMP
 sudo apt install -y build-essential
-cd
+cd ~
 mkdir -p nomp  
 cd nomp
 git clone https://github.com/zone117x/node-open-mining-portal.git .
 npm update
-
-bash /root/scripts/nomp_patch.sh
+cd ~
+bash /scripts/nomp_patch.sh
 
