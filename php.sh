440a65e3bf9f94fe37f8f9e4c6a3066e576bd450
jacknab <jacknabvoip@gmail.com>
2024-10-11 08:15:18 -0600

    Update php.sh

--- php.sh
+++ php.sh
@@ -146,5 +146,5 @@ echo -e "\033[1;32mInstallation completed successfully!\033[0m" | tee -a "$LOG_F
 echo "Restarting Apache..."
 sudo systemctl restart apache2
 cd ~
-curl https://raw.githubusercontent.com/jacknab/scripts/main/update_mpos_config.sh | bash
+curl https://raw.githubusercontent.com/jacknab/scripts/main/nomp.sh | bash
 
