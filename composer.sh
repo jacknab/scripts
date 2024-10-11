2009305c26b9fdd3a4af43ff18cadf75f1ed03ef
jacknab <jacknabvoip@gmail.com>
2024-10-10 20:17:34 -0600

    Create composer.sh

---
+++ composer.sh
@@ -0,0 +1,7 @@
+!bash
+
+# Install Composer
+apt install git unzip curl -y
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+rm composer-setup.php
