1adea1a6bb6d489d2e84c26bf81f872f3e26f877
jacknab <jacknabvoip@gmail.com>
2024-10-16 15:41:25 -0600

    Update nomp_patch.sh

--- nomp_patch.sh
+++ nomp_patch.sh
@@ -9,10 +9,10 @@
 set -x
 cd /root/nomp/libs
 sudo rm profitSwitch.js
-wget -4 https://raw.githubusercontent.com/jacknab/scripts/main/profitSwitch.js
+wget http://raw.githubusercontent.com/jacknab/scripts/main/profitSwitch.js
 cd /root/nomp/node_modules/stratum-pool/lib
 sudo rm pool.js
-wget -4 https://raw.githubusercontent.com/jacknab/scripts/main/pool.js
+wget http://raw.githubusercontent.com/jacknab/scripts/main/pool.js
 cd ~
 echo "Replacements completed successfully!"
 
