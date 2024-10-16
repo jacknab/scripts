0797700ba10727ddd16b5b6d4af138e4aa080dc9
jacknab <jacknabvoip@gmail.com>
2024-10-15 20:22:09 -0600

    Update php.sh

--- php.sh
+++ php.sh
@@ -70,10 +70,10 @@ sudo apt -y install php7.4
 sudo apt -y install memcached php7.4-memcached php7.4-mysql php7.4-cli php7.4-curl php7.4-mbstring php7.4-xml php7.4-zip php7.4-gd php7.4-json libapache2-mod-php7.4 curl
 sudo apt-get -y install php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath
 # php7.4 version
-sudo apt-get install php7.4-mbstring php7.4-xml
-sudo apt-get install php-mbstring php-xml php-mysql php-json php-curl php-zip
+sudo apt-get -y install php7.4-mbstring php7.4-xml
+sudo apt-get -y install php-mbstring php-xml php-mysql php-json php-curl php-zip
 # common PHP extensions
-sudo apt-get install php-mbstring php-json php-curl php-xml php-mysql php-mcrypt php-zip
+sudo apt-get -y install php-mbstring php-json php-curl php-xml php-mysql php-mcrypt php-zip
 
 # Step 3: Install Python 2.7
 # Install python 2.7
@@ -81,18 +81,18 @@ sudo apt-get install python2.7
 sudo ln -s /usr/bin/python2.7 /usr/bin/python
 wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
 sudo python2.7 get-pip.py
-sudo apt-get install python-pip
-sudo apt-get install libmysqlclient-dev
-sudo apt-get install python2-dev
-sudo apt-get install python2.7-dev build-essential
-sudo apt-get install python-setuptools
-pip install python-memcached
-sudo apt-get install python-memcached
-sudo apt-get install libmemcached-dev python-dev python-setuptools
+sudo apt-get install python-pip -y
+sudo apt-get install libmysqlclient-dev -y
+sudo apt-get install python2-dev -y
+sudo apt-get install -y python2.7-dev build-essential
+sudo apt-get install python-setuptools -y
+pip install python-memcached -y
+sudo apt-get -y install python-memcached
+sudo apt-get -y install libmemcached-dev python-dev python-setuptools 
 sudo pip install pylibmc
 
 # Python dependencies
-sudo apt-get install python-twisted python-mysqldb python-dev python-setuptools python-memcache python-simplejson
+sudo apt-get -y install python-twisted python-mysqldb python-dev python-setuptools python-memcache python-simplejson
 
 
 # Set phpMyAdmin selections using the retrieved MySQL root password
