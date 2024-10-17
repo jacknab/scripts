9c818e599c7291d6977d6247ebb4c09507aea53e
jacknab <jacknabvoip@gmail.com>
2024-10-16 18:06:51 -0600

    Update mundo.sh

--- mundo.sh
+++ mundo.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install build-essential libtool autoconf automake pkg-config libssl-dev libboost-all-dev libdb++-dev libminiupnpc-dev libgmp-dev
-sudo apt-get install libseccomp-dev
+sudo apt-get -y install build-essential libtool autoconf automake pkg-config libssl-dev libboost-all-dev libdb++-dev libminiupnpc-dev libgmp-dev
+sudo apt-get -y install libseccomp-dev
 sudo apt-get update
-sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libboost-all-dev libevent-dev
-sudo apt-get install libminiupnpc-dev libgmp-dev libseccomp-dev libcap-dev
+sudo apt-get -y install build-essential libtool autotools-dev automake pkg-config libssl-dev libboost-all-dev libevent-dev
+sudo apt-get -y install libminiupnpc-dev libgmp-dev libseccomp-dev libcap-dev
 pkg-config --modversion libseccomp
 sudo apt-get install libcap-dev
-sudo apt-get install g++-mingw-w64-i686 mingw-w64-i686-dev g++-mingw-w64-x86-64 mingw-w64-x86-64-dev curl libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
-sudo apt-get install g++-arm-linux-gnueabihf curl
+sudo apt-get -y install g++-mingw-w64-i686 mingw-w64-i686-dev g++-mingw-w64-x86-64 mingw-w64-x86-64-dev curl libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+sudo apt-get -y install g++-arm-linux-gnueabihf curl
 
 git clone https://github.com/jacknab/MundoTeam_Core.git
 cd /root/MundoTeam_Core
