4196f9add407937817e8eb1f95b57ae79262caf8
jacknab <jacknabvoip@gmail.com>
2024-11-09 20:56:03 -0700

    Add files via upload

---
+++ fork.sh
@@ -0,0 +1,142 @@
+#!/usr/bin/env bash
+
+//sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
+//sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+//sudo apt-get install libminiupnpc-dev
+//sudo apt-get install libzmq3-dev
+
+//sudo add-apt-repository ppa:bitcoin/bitcoin
+//sudo apt-get update
+//sudo apt-get install libdb4.8-dev libdb4.8++-dev
+
+git clone https://github.com/litecoin-project/litecoin.git mycoin
+cd mycoin
+
+
+
+find ./ -type f -readable -writable -exec sed -i "s/Hugocoin/Hugocoin/g" {} \;
+find ./ -type f -readable -writable -exec sed -i "s/HugoCoin/HugoCoin/g" {} \;
+find ./ -type f -readable -writable -exec sed -i "s/HUGO/HUGO/g" {} \;
+find ./ -type f -readable -writable -exec sed -i "s/hugocoin/hugocoin/g" {} \;
+find ./ -type f -readable -writable -exec sed -i "s/hugocoind/hugocoind/g" {} \;
+
+
+find ./ -type f -readable -writable -exec sed -i "s/lites/HUGO/g" {} \;
+find ./ -type f -readable -writable -exec sed -i "s/photons/HUGO/g" {} \;
+
+
+
+find ./ -type f -readable -writable -exec sed -i "s/9333/9666/g" {} \;  // change default port
+
+
+
+
+cd ${lite-coin-fork}
+nano src/chainparams.cpp
+    consensus.nSubsidyHalvingInterval = 11; //maximum coin supply
+    
+    
+    pchMessageStart[0] = 0xd0;
+    pchMessageStart[1] = 0xe1;
+    pchMessageStart[2] = 0xf5;
+    pchMessageStart[3] = 0xec;
+    
+    
+    base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,35);
+
+    base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1,35);
+
+    base58Prefixes[EXT_PUBLIC_KEY] = {0xff, 0x88, 0xB2, 0x1E};
+
+    base58Prefixes[EXT_SECRET_KEY] = {0xff, 0x88, 0xAD, 0xE4};
+    
+    //comment out  checkpointData block
+       
+    
+nano src/validation.cpp
+      CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
+    {
+      int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
+    // Force block reward to zero when right shift is undefined.
+    if (halvings >= 64)
+        return 0;
+
+    CAmount nSubsidy = 100 * COIN;
+    // Subsidy is cut in half every 210,000 blocks which will occur approximate$
+
+    if(nHeight < 11)
+    {
+        nSubsidy = 100000 * COIN;
+    }
+
+    //nSubsidy >>= halvings;
+    return nSubsidy;
+
+    }
+
+
+nano src/amount.h
+
+    static const CAmount COIN = 100000000;   // coin value
+    static const CAmount CENT = 1000000;      // how far coin can be splited
+    static const CAmount MAX_MONEY = 84000000 * COIN;   // total number of coin
+    
+    
+git clone  https://github.com/lhartikk/GenesisH0
+cd GenesisH0
+python genesis.py -a scrypt -z "BBC NEWS 20/Dec/2017 Bitcoin Cash deals frozen as insider trading is probed" -p "040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9" -t 1513784917 -n 2084524493
+
+//change 12 word for your convence
+
+
+cd ${litcoin-fork-root}
+
+./autogen.sh
+./configure  --without-gui  //use without-gui parameter to exclude qt stuff
+
+
+make   // wait for upto an hour if you are getting error on doc/man
+
+make install
+
+cd src
+strip hugocoind
+cd $HOME
+
+hugocoind
+
+cd .hugocoin
+
+nano .hugocoin.conf
+    server=1
+    rpcuser=user
+    rpcpassword=password
+    
+cd ${litcoin-fork-root}
+
+
+chmod 0600 hugocoin.conf
+
+
+// depoly second instance do same procedures, change in .hugocoin.conf
+
+  addnode=ip-address-of-other-node:port-no   // add n number of addnode to connect to network
+  listen=1
+  
+  hugocoind  -deprecatedrpc=accounts // run 
+
+
+hugocoin-cli getblockchaininfo 
+
+nano generate.sh
+
+#!/bin/bash
+echo "Mining... Press [CTRL+C] to stop"
+while :
+do
+  tetherwin-cli generate 1
+done
+
+ chmod +x generate.sh
+ 
+ ./generate.sh
---
+++ hugo.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+# Exit script on error
+set -e
+
+# Variables
+REPO_URL="https://github.com/litecoin-project/litecoin.git"
+COIN_NAME="Hugocoin"
+TICKER="HUGO"
+TIMESTAMP=$(date +%s)  # Get current timestamp
+NONCE="123456"          # Update to your desired nonce
+WALLET_ADDRESS_PREFIX="h"  # First letter of the wallet address
+FRAC_NAME="HUGO"        # Name for fractions of the coin
+PORT_BASE=20100         # Base port number for the coin
+P2P_PORT=$((PORT_BASE + 1))  # P2P port
+RPC_PORT=$((PORT_BASE + 2))  # RPC port
+
+# Check and install dependencies
+echo "Installing required dependencies..."
+sudo apt-get update
+sudo apt-get install -y \
+    build-essential \
+    libtool \
+    autotools-dev \
+    automake \
+    pkg-config \
+    libssl-dev \
+    libboost-all-dev \
+    libevent-dev \
+    libminiupnpc-dev \
+    libzmq3-dev \
+    qtbase5-dev \
+    qtchooser \
+    qt5-qmake \
+    qtbase5-dev-tools \
+    libprotobuf-dev \
+    protobuf-compiler \
+    python2 \
+    python2-dev \
+    python-setuptools
+
+# Install pip2
+echo "Installing pip2..."
+wget -4 https://bootstrap.pypa.io/pip/2.7/get-pip.py
+python2 get-pip.py
+
+# Remove existing litecoin directory if it exists
+if [ -d "litecoin" ]; then
+    echo "Removing existing Hugocoin directory..."
+    rm -rf litecoin
+fi
+
+# Clone the Hugocoin repository (using the 0.18 branch)
+echo "Cloning Hugocoin repository..."
+git clone -b 0.21 http://github.com/litecoin-project/litecoin.git
+cd litecoin || exit
+
+# Make autogen.sh and genbuild.sh executable
+echo "Setting execute permissions on autogen.sh and genbuild.sh..."
+chmod +x autogen.sh share/genbuild.sh
+
+# Download and run genesis.py
+echo "Downloading genesis.py..."
+wget -4 https://raw.githubusercontent.com/lhartikk/GenesisH0/master/genesis.py
+chmod +x genesis.py
+
+# Generate genesis block hash and Merkle root
+echo "Generating genesis block hash and Merkle root..."
+pip install scrypt==0.8.6
+pip2 install scrypt construct==2.5.2
+GENESIS_OUTPUT=$(python2 genesis.py -a scrypt -z "CNN 08/11/2024 Donald Trump become president again" -p "040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9" -t 1317972665 -n $NONCE)
+
+# Extract the genesis hash and merkle root from the output
+GENESIS_HASH=$(echo "$GENESIS_OUTPUT" | grep "Genesis Block Hash:" | awk '{print $NF}')
+MERKLE_ROOT=$(echo "$GENESIS_OUTPUT" | grep "Merkle Root:" | awk '{print $NF}')
+
+# Modify src/chainparams.cpp
+echo "Modifying src/chainparams.cpp..."
+sed -i "s/strNetworkID = \"test\";/strNetworkID = \"hugocoin\";/g" src/chainparams.cpp
+sed -i "s/The Times 05\/Nov\/2024 Trump becomes President again/$COIN_NAME Timestamp Here/g" src/chainparams.cpp
+sed -i "s/genesis.nTime = 1231006505;/genesis.nTime = $TIMESTAMP;/g" src/chainparams.cpp
+sed -i "s/genesis.nNonce = 2083236893;/genesis.nNonce = $NONCE;/g" src/chainparams.cpp
+sed -i "s/genesis.nBits = 0x1d00ffff;/genesis.nBits = 0x1e0fffff;/g" src/chainparams.cpp
+sed -i "s/pchMessageStart\[0\] = 0x4c;/pchMessageStart\[0\] = 0x42;/g" src/chainparams.cpp
+sed -i "s/pchMessageStart\[0\] = 0x4c;/pchMessageStart\[0\] = 0x68;/g" src/chainparams.cpp  # Update for H
+sed -i "s/static MapCheckpoints checkpoints = {/static MapCheckpoints checkpoints = {\n    { 0, uint256(\"$GENESIS_HASH\") }/g" src/validation.cpp
+
+# Set block reward to 2000 and disable halving
+echo "Setting block reward to 2000 and disabling halving..."
+sed -i "s/consensus.nSubsidy = .*;/consensus.nSubsidy = 2000 * COIN;/g" src/chainparams.cpp
+sed -i "s/consensus.nHalvingInterval = .*;/consensus.nHalvingInterval = 999999999;/g" src/chainparams.cpp  # Set a very high number to prevent halving
+
+# Modify src/chainparams.h
+echo "Modifying src/chainparams.h..."
+sed -i "s/static const std::string strNetworkID = \"test\";/static const std::string strNetworkID = \"hugocoin\";/g" src/chainparams.h
+sed -i "s/static const std::string COIN_NAME = \"Hugocoin\";/static const std::string COIN_NAME = \"$COIN_NAME\";/g" src/chainparams.h
+sed -i "s/static const std::string FRAC_NAME = \"Lites\";/static const std::string FRAC_NAME = \"$FRAC_NAME\";/g" src/chainparams.h
+
+# Remove DNS seeds and seed nodes
+echo "Removing DNS seeds and seed nodes..."
+sed -i '/vSeeds.emplace_back/d' src/chainparams.cpp
+# Commenting out the removal of pnSeed6_main to prevent build errors
+# sed -i '/pnSeed6_main/d' src/chainparamsseeds.h  
+
+# Set minimum chain work to 0x00
+echo "Setting minimum chain work to 0x00..."
+sed -i "s/consensus.nMinimumChainWork = .*;/consensus.nMinimumChainWork = uint256S(\"0x00\");/g" src/chainparams.cpp
+
+# Change default port to 4666
+echo "Changing default port to 4666..."
+find ./ -type f -readable -writable -exec sed -i "s/9333/9666/g" {} \;
+
+# Run autogen and configure scripts
+echo "Running autogen.sh..."
+./autogen.sh
+
+echo "Configuring build..."
+./configure --with-incompatible-bdb
+
+# Compile the code
+echo "Compiling the code..."
+make -j$(nproc)  # Compile using all available CPU cores
+
+# Rename the directory to Hugocoin
+echo "Renaming directory to Hugocoin..."
+cd .. 
+mv litecoin hugocoin
+
+echo "$COIN_NAME has been created with the ticker $TICKER!"
+echo "Current timestamp: $TIMESTAMP"
+echo "Nonce value: $NONCE"
+echo "P2P Port: $P2P_PORT"
+echo "RPC Port: $RPC_PORT"
