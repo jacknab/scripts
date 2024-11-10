b4a254157cba3def5ab6263cfdcd52896f1b1935
jacknab <jacknabvoip@gmail.com>
2024-11-09 21:02:50 -0700

    Update fork.sh

--- fork.sh
+++ fork.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-//sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
-//sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
-//sudo apt-get install libminiupnpc-dev
-//sudo apt-get install libzmq3-dev
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
+sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+sudo apt-get install libminiupnpc-dev
+sudo apt-get install libzmq3-dev
 
-//sudo add-apt-repository ppa:bitcoin/bitcoin
-//sudo apt-get update
-//sudo apt-get install libdb4.8-dev libdb4.8++-dev
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev
 
-git clone https://github.com/litecoin-project/litecoin.git mycoin
-cd mycoin
+git clone https://github.com/litecoin-project/litecoin.git
+
 
 
 
