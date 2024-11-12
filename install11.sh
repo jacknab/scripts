37ca3bf74090879d312cd176cc1e9b6edcd98521
jacknab <jacknabvoip@gmail.com>
2024-11-12 02:29:19 -0700

    Update install11.sh

--- install11.sh
+++ install11.sh
@@ -37,7 +37,7 @@ sudo apt update
 # MYSQL_ROOT_PASSWORD=$(openssl rand -base64 12)
 
 # Save the password to a text file
-echo "1825Logan305!" | sudo tee /mysql_information.txt > /dev/null
+# echo "1825Logan305!" | sudo tee /mysql_information.txt > /dev/null
 
 # Step 9: Set MySQL root password in debconf before installing
 echo "mysql-server mysql-server/root_password password 1825Logan305!" | sudo debconf-set-selections
@@ -57,5 +57,5 @@ rm -f mysql-apt-config_0.8.18-1_all.deb
 echo "MySQL root password: 1825Logan305!"
 
 cd ~
-curl https://raw.githubusercontent.com/jacknab/scripts/main/php1.sh | bash
+curl https://raw.githubusercontent.com/jacknab/scripts/main/php0.sh | bash
 
