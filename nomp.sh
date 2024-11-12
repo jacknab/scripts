2328302ff1f66b2ed534450bf02addb502e67461
jacknab <jacknabvoip@gmail.com>
2024-11-12 03:38:20 -0700

    Update nomp.sh

--- nomp.sh
+++ nomp.sh
@@ -6,21 +6,6 @@
 # curl https://raw.githubusercontent.com/jacknab/scripts/main/install.sh | bash
 #
 #########################################################
-set -x
-#sudo DEBIAN_FRONTEND=noninteractive apt remove -y nodejs npm
-# sudo apt remove -y nodejs npm
-#sudo DEBIAN_FRONTEND=noninteractive apt purge -y nodejs npm
-# sudo apt purge -y nodejs npm
-#sudo apt autoremove
-#sudo rm -rf /usr/local/node-v0.10.48
-#sudo rm /usr/bin/node
-#sudo rm /usr/bin/npm
-
-#wget -4 https://nodejs.org/dist/v0.10.48/node-v0.10.48-linux-x64.tar.gz
-#tar -xzf node-v0.10.48-linux-x64.tar.gz
-#sudo mv node-v0.10.48-linux-x64 /usr/local/node-v0.10.48
-#sudo ln -s /usr/local/node-v0.10.48/bin/node /usr/bin/node
-#sudo ln -s /usr/local/node-v0.10.48/bin/npm /usr/bin/npm
 
 # Install Nodejs
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
@@ -33,9 +18,8 @@ sudo apt install -y nodejs
 # Install NOMP
 sudo apt install -y build-essential
 cd ~
-mkdir nomp
+git clone https://github.com/nomp/node-open-mining-portal.git nomp
 cd nomp
-git clone https://github.com/nomp/node-open-mining-portal.git .
 # nvm install 16
 # nvm use 16
 # npm rebuild --force
