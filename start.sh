22f6b2152377cb0b7e66fd69ba56fd91a6377b65
jacknab <jacknabvoip@gmail.com>
2024-11-12 04:07:02 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,39 +1,40 @@
-#!/usr/bin/env bash
-#########################################################
-# Created by jacknab for php-mpos and nomp pool...
-# This script allows you to choose the OS to run specific setup scripts
-# like this:
-# curl https://raw.githubusercontent.com/jacknab/scripts/main/start.sh | bash
-#
-#########################################################
+#!/bin/bash
 
-# Sleep for a second to make sure the terminal gets time to load
-sleep 1
+# Check the operating system
+OS=$(lsb_release -d | awk -F"\t" '{print $2}' | awk '{print $1}')
 
-echo "Please select your operating system:"
-echo "[1] Debian 11"
-echo "[2] Ubuntu 20.04"
-echo "[3] Ubuntu 22.04"
+# Function to install on Debian 11
+install_debian11() {
+    echo "Detected Debian 11. Running Debian 11 specific installation script..."
+    # Replace this with the actual Debian 11 script command or wget to a file
+    curl -sL https://raw.githubusercontent.com/jacknab/scripts/main/debian11_install.sh | bash
+}
 
-# Read user input
-read -p "Enter your choice [1, 2, or 3]: " os_choice
+# Function to install on Ubuntu 22.04
+install_ubuntu2204() {
+    echo "Detected Ubuntu 22.04. Running Ubuntu 22.04 specific installation script..."
+    # Replace this with the actual Ubuntu 22.04 script command or wget to a file
+    curl -sL https://raw.githubusercontent.com/jacknab/scripts/main/ubuntu22_install.sh | bash
+}
 
-# Validate input and execute corresponding script
-case $os_choice in
-    1)
-        echo "You selected Debian 11"
-        curl https://raw.githubusercontent.com/jacknab/scripts/main/install-debian11.sh | bash
-        ;;
-    2)
-        echo "You selected Ubuntu 20.04"
-        curl https://raw.githubusercontent.com/jacknab/scripts/main/install-ubuntu20.sh | bash
-        ;;
-    3)
-        echo "You selected Ubuntu 22.04"
-        curl https://raw.githubusercontent.com/jacknab/scripts/main/install-ubuntu22.sh | bash
-        ;;
-    *)
-        echo "Invalid selection. Please select 1, 2, or 3."
+# Check the OS version and execute the corresponding function
+if [[ "$OS" == "Ubuntu" ]]; then
+    VERSION=$(lsb_release -r | awk '{print $2}')
+    if [[ "$VERSION" == "22.04" ]]; then
+        install_ubuntu2204
+    else
+        echo "This script is designed for Ubuntu 22.04. Exiting."
         exit 1
-        ;;
-esac
+    fi
+elif [[ "$OS" == "Debian" ]]; then
+    VERSION=$(lsb_release -r | awk '{print $2}')
+    if [[ "$VERSION" == "11" ]]; then
+        install_debian11
+    else
+        echo "This script is designed for Debian 11. Exiting."
+        exit 1
+    fi
+else
+    echo "Unsupported operating system. This script supports only Ubuntu 22.04 or Debian 11."
+    exit 1
+fi
