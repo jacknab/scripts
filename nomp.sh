c3ae8124d9b745bf378c247a706544c7bd1d7845
jacknab <jacknabvoip@gmail.com>
2024-11-15 21:14:31 -0700

    Update nomp.sh

--- nomp.sh
+++ nomp.sh
@@ -14,15 +14,11 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-#nvm install 14.13.1
-#nvm alias default 14.13.1
-
+nvm install 14.13.1
+nvm alias default 14.13.1
 
 # Install NOMP
 sudo apt install -y build-essential
-sudo mkdir nomp
-#git clone https://github.com/nomp/node-open-mining-portal.git nomp
-cd nomp
 wget https://raw.githubusercontent.com/jacknab/scripts/main/nomp_.tar.gz
 tar -xvzf nomp_.tar.gz
 sudo apt-get install -y redis-server
