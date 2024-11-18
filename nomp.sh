df46a8b6b6cffe624cb4031827bf2042219404d5
jacknab <jacknabvoip@gmail.com>
2024-11-18 06:50:50 -0700

    Update nomp.sh

--- nomp.sh
+++ nomp.sh
@@ -8,14 +8,22 @@
 #########################################################
 
 # Install Nodejs
-curl -fsSL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt install -y nodejs
+wget https://nodejs.org/dist/v10.19.0/node-v10.19.0-linux-x64.tar.xz
+tar -xf node-v10.19.0-linux-x64.tar.xz
+sudo mv node-v10.19.0-linux-x64 /usr/local/node-10.19.0
+sudo ln -s /usr/local/node-10.19.0/bin/node /usr/local/bin/node
+sudo ln -s /usr/local/node-10.19.0/bin/npm /usr/local/bin/npm
+sudo npm install -g npm@latest
+sudo apt-get autoremove
+sudo apt-get clean
+
+#sudo apt install -y nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install 10.19.0
-nvm alias default 10.19.0
+#nvm install 10.19.0
+#nvm alias default 10.19.0
 
 # Install NOMP
 sudo apt install -y build-essential
