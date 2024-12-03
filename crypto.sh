8e4161de2763a5ad0a225cd8ef2b3a3a09e49765
jacknab <jacknabvoip@gmail.com>
2024-12-03 13:41:50 -0700

    Update crypto.sh

--- crypto.sh
+++ crypto.sh
@@ -4,7 +4,7 @@
 # Created by jacknab for php-mpos and nomp pool...
 # This script is intended to be run on Debian 11
 # like this:
-# curl https://raw.githubusercontent.com/jacknab/scripts/main/install.sh | bash
+# curl https://raw.githubusercontent.com/jacknab/scripts/main/crypto.sh | bash
 #
 #########################################################
 
